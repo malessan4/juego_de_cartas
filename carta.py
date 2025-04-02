7a504dcd90301b438fe842955cc6013da7636f02
Matias Alessandrello <matialess@gmail.com>
2025-04-02 20:37:22 -0300

    definiendo clase Carta

--- carta.py
+++ carta.py
@@ -1,7 +1,11 @@
 class Carta:
     listaDePalos = ['Treboles', 'Diamantes', 'Corazones', 'Picas']
     
+    listaDeValores = ['nada', 'As', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Sota', 'Reina', 'Rey']
+    
     def __init__(self, palo = 0, valor = 0):
         self.palo = palo
         self.valor = valor
         
+    def __str__(self):
+        return (self.listaDeValores[self.valor] + " de " + self.listaDePalos[self.palo])
