4bde7367f3ba1693d32b358c879119e023904741
Matias Alessandrello <matialess@gmail.com>
2025-04-02 20:31:08 -0300

    Primer commit, armando con POO un juego de cartas

---
+++ carta.py
@@ -0,0 +1,7 @@
+class Carta:
+    listaDePalos = ['Treboles', 'Diamantes', 'Corazones', 'Picas']
+    
+    def __init__(self, palo = 0, valor = 0):
+        self.palo = palo
+        self.valor = valor
+        
